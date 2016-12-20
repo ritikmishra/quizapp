8334a44172d8e4bd3d7e96630551ef2a4c94e68e
Ritik Mishra <ritik.mishra314@gmail.com>
2016-12-19 22:04:42 -0600

    added short answer among other things

--- server.py
+++ server.py
@@ -9,17 +9,16 @@ quizjson = json.load(quizjsonfile)['quizzes']
 quiztemplate = template.Template(quiztemplatefile.read())
 print(quizjson)
 def paramsfromrequest(request):
-    """ Takes in an HTTPRequest object and returns the parameters(the things after the question mark paired by equal signs and separated by ampersands) from the URI"""
-    param_string = request.uri.split("?")[1] #gets the part after the question mark as a string
-    # print(param_string)
-    params_list = param_string.split("&") # gives a list containing every pair=of request=paramaters in=a list=like that
-    # print(params_list)
+    """ Makes HTTPRequest nice to me"""
+    params = request.arguments
     params_dict = {}
-    for pair in params_list: #loops through list and makes every key=value pair into a key: value pair in the dictionary
-        key = pair.split("=")[0]
-        value = pair.split("=")[1]
-        params_dict[key] = value
-    # print(params_dict)
+    for key, value in request.arguments.items():
+        if len(value) == 1:
+            params_dict[key] = value[0].decode('UTF-8')
+        else:
+            params_dict[key] = []
+            for part in value:
+                params_dict[key].append(part.decode('UTF-8'))
     return params_dict
 
 
@@ -31,18 +30,24 @@ class SecondaryHandler(tornado.web.RequestHandler):
     def get(self):
         try:
             self.params = paramsfromrequest(self.request)
+            for key, value in list(self.params.items()):
+                print("Arg: ",key)
+                print("Value: ",value)
             self.write(quiztemplate.generate(quiz=quizjson[self.params["quiz-id"]]))
         except KeyError:
             self.write("Quiz not found")
+    def post(self):
+        pass
+        #TODO: make it add an entry into quizzes.json
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/path", SecondaryHandler),
+        (r"/quiz", SecondaryHandler),
     ])
 
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
-    print("Listening on port " + str(port))
+    print("\n\n\nListening on port " + str(port))
     tornado.ioloop.IOLoop.current().start()
