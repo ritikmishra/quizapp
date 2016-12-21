6f4330c9cc042cf81a77d46ad61d16d9e75791ca
Ritik Mishra <ritik.mishra314@gmail.com>
2016-12-20 23:32:00 -0600

    minor changes

--- server.py
+++ server.py
@@ -2,10 +2,11 @@ import tornado.ioloop
 import tornado.web
 import tornado.template as template
 import json
+import random
 port = 8888
 quizjsonfile = open("./quizzes.json", "r+")
 quiztemplatefile = open("./quiztemplate.html", "r+")
-quizjson = json.load(quizjsonfile)['quizzes']
+quizjson = json.load(quizjsonfile)
 quiztemplate = template.Template(quiztemplatefile.read())
 print(quizjson)
 def paramsfromrequest(request):
@@ -22,28 +23,67 @@ def paramsfromrequest(request):
     return params_dict
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+class AnswerHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        self.params = paramsfromrequest(self.request)
+        for key, value in list(self.params.items()):
+            print("Arg: ",key)
+            print("Value: ",value)
+    def post(self):
+        self.write("data recieved")
+
+class MakeQuiz(tornado.web.RequestHandler):
+    def prepare(self):
+        self.params = paramsfromrequest(self.request)
+        for key, value in list(self.params.items()):
+            print("Arg: ",key)
+            print("Value: ",value)
+    def post(self):
+        print(self.params)
+        self.quizname = random.randrange(0, 999999999999)
+        quizjson[self.quizname] = {
+            "title": "My Second Quiz",
+            "multiple_choice":[{
+                    "text": "Is this a mutliple choice question?",
+                    "options": ["Yes","No","Perhaps"],
+                    "answer": "Yes"
+                },
+                {
+                    "text": "What is microphone?",
+                    "options": ["False","True"],
+                    "answer": "Yes"
+                }],
+            "short_answer":[
+            {
+                "text": "Is this a mutliple choice question?",
+                "keywords": ["quiz", "contains", "elements", "both", "multiple", "choice", "short", "answer", "bubbles", "checkbox", "textbox"],
+                "answer": "Yes"
+            }]
+        }
+
+        print(self.quizname)
+        self.write(quiztemplate.generate(quiz=quizjson[self.quizname]))
+        #TODO: make it add an entry into quizzes.json
 
 class SecondaryHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        self.params = paramsfromrequest(self.request)
+        for key, value in list(self.params.items()):
+            print("Arg: ",key)
+            print("Value: ",value)
     def get(self):
         try:
-            self.params = paramsfromrequest(self.request)
-            for key, value in list(self.params.items()):
-                print("Arg: ",key)
-                print("Value: ",value)
+            print(self.params["quiz-id"])
             self.write(quiztemplate.generate(quiz=quizjson[self.params["quiz-id"]]))
         except KeyError:
             self.write("Quiz not found")
-    def post(self):
-        pass
-        #TODO: make it add an entry into quizzes.json
+
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/", AnswerHandler),
         (r"/quiz", SecondaryHandler),
+        (r"/upload", MakeQuiz),
     ])
 
 if __name__ == "__main__":
