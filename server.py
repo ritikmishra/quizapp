6d9bbcf8dc087bfa6fe5ea9e71fe2763e034e2e9
Ritik Mishra <ritik.mishra314@gmail.com>
2016-12-21 23:33:33 -0600

    Uploading quizzes with POST request to /upload works now

--- server.py
+++ server.py
@@ -3,24 +3,16 @@ import tornado.web
 import tornado.template as template
 import json
 import random
+from shortanswer import paramsfromrequest
+from shortanswer import stringtojson
 port = 8888
-quizjsonfile = open("./quizzes.json", "r+")
-quiztemplatefile = open("./quiztemplate.html", "r+")
-quizjson = json.load(quizjsonfile)
-quiztemplate = template.Template(quiztemplatefile.read())
+
+with open("./quizzes.json", "r+") as quizjsonfile:
+    quizjson = json.load(quizjsonfile)
+with open("./quiztemplate.html", "r+") as quiztemplatefile:
+    quiztemplate = template.Template(quiztemplatefile.read())
+
 print(quizjson)
-def paramsfromrequest(request):
-    """ Makes HTTPRequest nice to me"""
-    params = request.arguments
-    params_dict = {}
-    for key, value in request.arguments.items():
-        if len(value) == 1:
-            params_dict[key] = value[0].decode('UTF-8')
-        else:
-            params_dict[key] = []
-            for part in value:
-                params_dict[key].append(part.decode('UTF-8'))
-    return params_dict
 
 
 class AnswerHandler(tornado.web.RequestHandler):
@@ -35,49 +27,37 @@ class AnswerHandler(tornado.web.RequestHandler):
 class MakeQuiz(tornado.web.RequestHandler):
     def prepare(self):
         self.params = paramsfromrequest(self.request)
+
         for key, value in list(self.params.items()):
             print("Arg: ",key)
             print("Value: ",value)
-    def post(self):
-        print(self.params)
-        self.quizname = random.randrange(0, 999999999999)
-        quizjson[self.quizname] = {
-            "title": "My Second Quiz",
-            "multiple_choice":[{
-                    "text": "Is this a mutliple choice question?",
-                    "options": ["Yes","No","Perhaps"],
-                    "answer": "Yes"
-                },
-                {
-                    "text": "What is microphone?",
-                    "options": ["False","True"],
-                    "answer": "Yes"
-                }],
-            "short_answer":[
-            {
-                "text": "Is this a mutliple choice question?",
-                "keywords": ["quiz", "contains", "elements", "both", "multiple", "choice", "short", "answer", "bubbles", "checkbox", "textbox"],
-                "answer": "Yes"
-            }]
-        }
 
+        self.quizdata = stringtojson(self.params["quiz"])
+    def post(self):
+        self.quizname = random.randrange(0, 999999999999)
+        print(self.quizdata)
+
+        quizjson[self.quizname] = self.quizdata
         print(self.quizname)
+        # self.write(quiztemplate.generate(quiz=quizjson[self.quizname]))
+        with open("./quizzes.json", "w") as quizjsonfile:
+            print(quizjson)
+            quizjsonfile.write(json.dumps(quizjson, indent=4, separators=(',', ': ')))
         self.write(quiztemplate.generate(quiz=quizjson[self.quizname]))
+        #yells at me for trying to send a list :(
         #TODO: make it add an entry into quizzes.json
 
 class SecondaryHandler(tornado.web.RequestHandler):
     def prepare(self):
         self.params = paramsfromrequest(self.request)
-        for key, value in list(self.params.items()):
-            print("Arg: ",key)
-            print("Value: ",value)
+        print(self.request.arguments)
+
     def get(self):
         try:
             print(self.params["quiz-id"])
-            self.write(quiztemplate.generate(quiz=quizjson[self.params["quiz-id"]]))
+            self.write(quiztemplate.generate(quiz=quizjson[self.params["quiz-id"]],id=self.params["quiz-id"]))
         except KeyError:
             self.write("Quiz not found")
-
 
 def make_app():
     return tornado.web.Application([
--- shortanswer.py
+++ shortanswer.py
@@ -2,8 +2,31 @@
 """
 This module was made for the QuizApp by Ritik Mishra.
 
-It defines the Answer class which has methods for seeing if an answer is correct.  
+It defines the Answer class which has methods for seeing if an answer is correct.
+Also has some other useful functions
 """
+import json
+
+def paramsfromrequest(request):
+    """ Makes HTTPRequest nice to me"""
+    params = request.arguments
+    params_dict = {}
+    for key, value in request.arguments.items():
+        if len(value) == 1:
+            params_dict[key] = value[0].decode('UTF-8')
+        else:
+            params_dict[key] = []
+            for part in value:
+                params_dict[key].append(part.decode('UTF-8'))
+    return params_dict
+
+def stringtojson(words):
+        print(words)
+        json_acceptable_string = words.replace("'", "\"")
+
+        dictionary = json.loads(json_acceptable_string)
+        return dictionary
+        # return words
 
 class Answer:
     def __init__(self, keywords, answer):
