3dbf348b914bdadeda17c7f5e15d8f802360e892
Ritik Mishra <ritik.mishra314@gmail.com>
2017-01-03 12:01:41 -0600

    hopefully it doesn't break fro all the self.'s

--- main.py
+++ main.py
@@ -40,33 +40,33 @@ class AnswerHandler(tornado.web.RequestHandler):
         self.params = paramsfromrequest(self.request)
         print("Answer Check Params \n #############################")
         print(self.params)
-    def post(self):
         with open("./quizzes.json", "r+") as quizjsonfile:
-            quizjson = json.load(quizjsonfile)
+            self.quizjson = json.load(quizjsonfile)
             quizjsonfile.close()
-        print(quizjson)
-        user_mc_ans = {}
-        q_mc_ans = []
-        q_sa_keywords = []
-        user_sa_ans = {}
+        print(self.quizjson)
+    def post(self):
+        self.user_mc_ans = {}
+        self.q_mc_ans = []
+        self.q_sa_keywords = []
+        self.user_sa_ans = {}
         for key, value in list(self.params.items()):
             if key != 'quiz-id':
                 if key[0:2] == 'mc':
-                    user_mc_ans[int(key[2:])] = value
+                    self.user_mc_ans[int(key[2:])] = value
                 elif key[0:2] == 'sa':
-                    user_sa_ans[int(key[2:])] = value
+                    self.user_sa_ans[int(key[2:])] = value
         try:
-            for question in list(quizjson[self.params["quiz-id"]]['multiple_choice']):
-                q_mc_ans.append(question["answer"])
+            for question in list(self.quizjson[self.params["quiz-id"]]['multiple_choice']):
+                self.q_mc_ans.append(question["answer"])
         except KeyError:
-            q_mc_ans = None
+            self.q_mc_ans = None
         try:
-            for question in list(quizjson[self.params["quiz-id"]]['short_answer']):
-                q_sa_keywords.append(question["keywords"])
+            for question in list(self.quizjson[self.params["quiz-id"]]['short_answer']):
+                self.q_sa_keywords.append(question["keywords"])
         except KeyError:
-            q_sa_keywords = None
-        self.checkans = Answer(q_sa_keywords, user_sa_ans, user_mc_ans, q_mc_ans)
-        print(user_sa_ans)
+            self.q_sa_keywords = None
+        self.checkans = Answer(self.q_sa_keywords, self.user_sa_ans, self.user_mc_ans, self.q_mc_ans)
+        print(self.user_sa_ans)
         # print(q_mc_ans)
         self.checked_mc = self.checkans.mc_check()
         self.checked_sa = self.checkans.sa_check()
@@ -75,31 +75,33 @@ class AnswerHandler(tornado.web.RequestHandler):
         print(self.checked_sa)
         print("Multiple Choice questions \n ###############################")
         print(self.checked_mc)
-        self.write(answertemplate.generate(quiz=quizjson[self.params["quiz-id"]],id=self.params["quiz-id"],mc_answers=self.checked_mc,sa_answers=self.checked_sa))
+        self.write(answertemplate.generate(quiz=self.quizjson[self.params["quiz-id"]],id=self.params["quiz-id"],mc_answers=self.checked_mc,sa_answers=self.checked_sa))
 class MakeQuiz(tornado.web.RequestHandler):
     def prepare(self):
         self.params = paramsfromrequest(self.request)
-
+        with open("./quizzes.json", "r+") as quizjsonfile:
+            self.quizjson = json.load(quizjsonfile)
+            quizjsonfile.close()
+        print(self.quizjson)
     def post(self):
-        global quizjson
         try:
             self.quizdata = stringtojson(self.params["quiz"])
             self.quizid = random.randrange(0, 999999999999)
-            seentitle = False
+            self.seentitle = False
             for key, value in list(self.quizdata.items()):
                 #check if there is a quiz title
                 print(key)
                 if key == "title":
-                    seentitle = True
-            if seentitle:
+                    self.seentitle = True
+            if self.seentitle:
                 print("New quiz uploaded: " + str(self.quizid) + " \n ###############")
                 print(self.quizdata)
-                quizjson[self.quizid] = self.quizdata
+                self.quizjson[self.quizid] = self.quizdata
                 with open("./quizzes.json", "w") as quizjsonfile:
-                    quizjsonfile.write(json.dumps(quizjson))
+                    json.dump(self.quizjson, quizjsonfile, ensure_ascii=False)
                     quizjsonfile.close()
                 self.redirect("https://codebreakquizapp.herokuapp.com/quiz?quiz-id=" + str(self.quizid))
-            elif not seentitle:
+            elif not self.seentitle:
                 self.write("Your quiz has no title, so we did not upload it to our server. ")
         except KeyError:
             self.write("We did not detect a quiz. Make sure that it was sent under the parameter of 'quiz'.")
@@ -110,17 +112,18 @@ class SecondaryHandler(tornado.web.RequestHandler):
             print(self.params["quiz-id"])
         except KeyError:
             self.params['quiz-id'] = None
-
-        print(self.request.arguments)
+        finally:
+            print(self.request.arguments)
+            with open("./quizzes.json", "r+") as quizjsonfile:
+                self.quizjson = json.load(quizjsonfile)
+                quizjsonfile.close()
+            print(self.quizjson)
     def get(self):
-        with open("./quizzes.json", "r+") as quizjsonfile:
-            quizjson = json.load(quizjsonfile)
-            quizjsonfile.close()
         if self.params['quiz-id'] == None:
             self.write(quizsearchtemplate.generate())
         else:
             try:
-                self.write(quiztemplate.generate(quiz=quizjson[self.params["quiz-id"]],id=self.params["quiz-id"]))
+                self.write(quiztemplate.generate(quiz=self.quizjson[self.params["quiz-id"]],id=self.params["quiz-id"]))
             except KeyError:
                 self.write(quiznotfoundtemplate.generate())
 class MainPageRedirHandler(tornado.web.RequestHandler):
@@ -129,9 +132,10 @@ class MainPageRedirHandler(tornado.web.RequestHandler):
 class MainPageHandler(tornado.web.RequestHandler):
     def get(self):
         with open("./quizzes.json", "r+") as quizjsonfile:
-            quizjson = json.load(quizjsonfile)
+            self.quizjson = json.load(quizjsonfile)
             quizjsonfile.close()
-        self.write(mainpagetemplate.generate(quizzes=quizjson))
+        print(self.quizjson)
+        self.write(mainpagetemplate.generate(quizzes=self.quizjson))
 
 def make_app():
     return tornado.web.Application([
